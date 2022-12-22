491b7b2640331a90c7d8a14654bfd55710e64af4
Lopushka <sv13yat@gmail.com>
2022-12-22 11:30:40 +0300

    Fin_EX_3_v2.0

--- EX_3/Program.cs
+++ EX_3/Program.cs
@@ -1,4 +1,29 @@
-﻿Console.WriteLine("Задайте размер массива.");
+﻿int Palindrome(int numder)
+{
+    char[] myArray = numder.ToString().ToCharArray();
+
+    char[] newArray = myArray.Reverse().ToArray();
+
+
+    for (var i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i].Equals(newArray[i]))
+        {
+            if (i == myArray.Length - 1)
+            {
+                return numder;
+            }
+
+        }
+
+    }
+    return 0;
+}
+
+
+
+
+Console.WriteLine("Задайте размер массива.");
 loop1:
 Console.Write("\nРазмер по вертикали число больше 0: ");
 int namder_y = Convert.ToInt32(Console.ReadLine());
@@ -8,18 +33,20 @@ Random random = new Random();
 
 if (namder_y > 0 && namder_x > 0)
 {
+    double[] myArray = new double[namder_x];
     int[,] TwoArray = new int[namder_y, namder_x];
     System.Console.WriteLine("Вывожу массив");
+
     for (int i = 0; i < TwoArray.GetLength(0); i++)
     {
         for (int j = 0; j < TwoArray.GetLength(1); j++)
         {
 
-            TwoArray[i, j] = random.Next(-10, 11);
+            TwoArray[i, j] = random.Next(100, 1000);
         }
     }
 
-    for (int i = 0; i < TwoArray.GetLength(0); i++)
+    for (int i = 0; i < TwoArray.GetLength(0); i++) // проверка массива на пол
     {
         for (int j = 0; j < TwoArray.GetLength(1); j++)
         {
@@ -29,16 +56,39 @@ if (namder_y > 0 && namder_x > 0)
         Console.WriteLine();
     }
 
-    Console.Write("Cреднее арифметическое элементов в каждом столбце");
-    Console.WriteLine();
+
+
+    for (int i = 0; i < TwoArray.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < TwoArray.GetLength(1); j++)
+        {
+            TwoArray[i, j] = Palindrome(TwoArray[i, j]);
+            Console.Write(TwoArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
     for (int i = 0; i < TwoArray.GetLength(1); i++)
     {
-        double arithmetic = 0;
+        double counter = 0;
+        int summer = 0;
         for (int j = 0; j < TwoArray.GetLength(0); j++)
         {
-            arithmetic = arithmetic + TwoArray[j, i];
+
+            if (TwoArray[j, i] > 1)
+            {
+                summer += TwoArray[j, i];
+                counter++;
+                myArray[i] = summer / counter;
+            }
         }
-        Console.Write(+arithmetic / TwoArray.GetLength(0) + "\t");
+    }
+
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i] + "\t");
     }
 }
 else
